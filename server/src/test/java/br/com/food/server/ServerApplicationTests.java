1912a271edcd6e48a3324b4eee55b654b83764bf
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-11-24 23:14:11 -0300

    FEAT(ServerApplicationTests): Adicionando anotação do @Eureka

--- server/src/test/java/br/com/food/server/ServerApplicationTests.java
+++ server/src/test/java/br/com/food/server/ServerApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.food.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
+@EnableEurekaServer
 class ServerApplicationTests {
 
 	@Test
