a7dbfc8e652774676b0501d823f25287f59f459b
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-06 01:55:39 -0300

    FEAT(AluraServiceStack): Adicionando configurações para o ambiente

--- infra-aws/src/main/java/com/myorg/AluraServiceStack.java
+++ infra-aws/src/main/java/com/myorg/AluraServiceStack.java
@@ -1,5 +1,6 @@
 package com.myorg;
 
+import software.amazon.awscdk.Fn;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ecs.Cluster;
@@ -7,6 +8,9 @@ import software.amazon.awscdk.services.ecs.ContainerImage;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.constructs.Construct;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AluraServiceStack extends Stack {
     public AluraServiceStack(final Construct scope, final String id, final Cluster cluster) {
@@ -16,21 +20,28 @@ public class AluraServiceStack extends Stack {
     public AluraServiceStack(final Construct scope, final String id, final StackProps props, final Cluster cluster) {
         super(scope, id, props);
 
+        Map<String, String> autenticacao= new HashMap<>();
+        autenticacao.put("SPRING_DATASOURCE_URL", "jdbc:mysql://" + Fn.importValue("pedidos-db-endpoint")
+                + ":3306/alurafood-pedidos?createDatabaseIfNotExist=true");
+        autenticacao.put("SPRING_DATASOURCE_USERNAME", "admin");
+        autenticacao.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("pedidos-db-senha"));
+
         ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
                 .serviceName("alura-service-ola")
-                .cluster(cluster)           // Required
-                .cpu(512)                   // Default is 256
-                .desiredCount(1)            // Default is 1
+                .cluster(cluster)
+                .cpu(512)
+                .desiredCount(1)
                 .listenerPort(8080)
                 .assignPublicIp(true)
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
-                                .image(ContainerImage.fromRegistry("jacquelineoliveira/ola:1.0"))
+                                .image(ContainerImage.fromRegistry("brdoliveira/pedidos-ms"))
                                 .containerPort(8080)
                                 .containerName("app_ola")
+                                .environment(autenticacao)
                                 .build())
-                .memoryLimitMiB(1024)       // Default is 512
-                .publicLoadBalancer(true)   // Default is false
+                .memoryLimitMiB(1024)
+                .publicLoadBalancer(true)
                 .build();
     }
 }
